2cf45d921987d638e291a4ee7de3bba543d689b4
Ohohsiol <118786550+Ohohsiol@users.noreply.github.com>
2023-02-25 19:53:04 +0800

    Update Mapping.py

--- spider1/Mapping.py
+++ spider1/Mapping.py
@@ -1,8 +1,8 @@
 import scrapy
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['http://172.18.58.80/multi']
-    def parse(self, response):
+    start_urls = ['http://172.18.58.80/multi'] #target URL
+    def parse(self, response): #used to extract info from webpage
         css_selector = 'img'
         for x in response.css(css_selector):
             newsel = '@src'
