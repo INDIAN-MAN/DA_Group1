69ce1bd89f3df239cd7ccf43977939b0d0b78df7
Ohohsiol <118786550+Ohohsiol@users.noreply.github.com>
2023-02-25 19:59:37 +0800

    Update Mapping.py

--- spider1/Mapping.py
+++ spider1/Mapping.py
@@ -3,7 +3,7 @@ class NewSpider(scrapy.Spider):
     name = "new_spider"
     start_urls = ['http://172.18.58.80/multi'] #target URL
     def parse(self, response): #used to extract info from webpage
-        css_selector = 'img'
+        css_selector = 'img'  #selects all image on the webpage using CSS
         for x in response.css(css_selector):
             newsel = '@src'
             yield {'Image Link': x.xpath(newsel).extract_first(), }
@@ -12,3 +12,4 @@ class NewSpider(scrapy.Spider):
         next_page = response.css(page_selector).extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+            
